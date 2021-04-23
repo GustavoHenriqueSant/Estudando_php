d5d196de7faa021f0abf4a39bd27e2e0ea88c596
GustavoHenriqueSant <gustavo.git@hotmail.com>
2021-04-23 20:58:43 -0300

    Adição do arquivo conceitos básicos php

---
+++ 01-Fundamentos_basicos_PHP.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fundamentos básicos</title>
+</head>
+<body>
+
+
+<?php
+//Comentário PHP
+/* comentário de amis de uma linha
+adasdadasdasdas
+sadadadasda */
+echo "Meu nome é Gustavo <br>";
+print "Minha idade é 20";
+?>
+
+</body>
+</html>
