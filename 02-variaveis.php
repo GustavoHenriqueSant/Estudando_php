669a3a4a7d9b65fc3efdc32ed17d55554b689ff3
GustavoHenriqueSant <gustavo.git@hotmail.com>
2021-04-23 21:11:25 -0300

    Criando arquivo 02variaveis.php mostrando sintaxe para declarar variáveis.

---
+++ 02-variaveis.php
@@ -0,0 +1,21 @@
+<?php
+
+// Variáveis de tipagem fraca, são definidas na hr da atribuição do valor da variável:
+$nome = "Gustavo Henrique";
+$idade = 20;
+$altura = 1.74;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+
+$nome = "Rodrigo Oliveira";
+$idade = 22;
+$altura = 1.89;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura";
+
+// Nomes válidos é igual a qualquer outra linguagem que vc já utilizou.
+
+//chamada direta no echo.
+$carro = "Hilux;"
+
+echo $carro;
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-## PHP
+# PHP
 
 Repositório criado para *aprender php*.
+
+## Sintaxe PHP:
+
+* // comentário aqui -- Fazer comentários de uma linha.
+* /* comentário aqui */ -- Fazer comentários de mais de uma linha.
+* print ""; ou echo ""; -- Mostrar na tela.
+* $name_variavel -- Declaração de variável.
